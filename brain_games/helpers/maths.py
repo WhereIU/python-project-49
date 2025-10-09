4e863c84faee81f7fb05325e7eb0e7dd8b83ddf1
Saint <max161125@gmail.com>
2025-10-09 18:32:38 +0300

    semantics fix

--- brain_games/helpers/maths.py
+++ brain_games/helpers/maths.py
@@ -7,7 +7,7 @@ def is_prime(num):
         return
     if num <= 3:
         return True
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
